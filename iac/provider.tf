6d183c3b033903103589081b6d923746bf3d7eb8
Arturo Burigo <burigoarturo3@gmail.com>
2025-06-09 23:33:27 -0300

    feat: setting up azure adls and uploading data from database

--- iac/provider.tf
+++ iac/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.3.0"
+      version = "~> 3.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
